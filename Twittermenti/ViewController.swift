3590819cd4ec0a0e651f1e321c0dcdcac64d6c64
Emmanuel Cuevas <emmanuelcu@gmail.com>
2021-01-29 20:37:58 -0600

    The twitter API is implemented in the app and is getting information from twitter.

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -7,15 +7,25 @@
 //
 
 import UIKit
+import SwifteriOS
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+        
+    let swifter = Swifter(consumerKey: "yADcIgShJ9in9ibOE6cuIfrz5", consumerSecret: "cfwAVD4TTCUWefwqP0IhEzvgMj8CcKZ5igLXLGXCI5ZGYFp5Gi")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended ) { (results, metadata) in
+            print(results)
+        } failure: { (error) in
+            print("There was an error with the Twitter API Request, \(error)")
+        }
+
         
     }
 
